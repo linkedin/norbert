2d201bc2daae58dca756115934872fdda1c1ea5d
Nick Hristov <nhristov@tidemark.com>
2014-10-28 12:24:34 -0700

    Removing Tidemark copyright

--- java-network/src/test/java/com/linkedin/norbert/javacompat/network/ConsistentHashPartitionedLoadBalancerTest.java
+++ java-network/src/test/java/com/linkedin/norbert/javacompat/network/ConsistentHashPartitionedLoadBalancerTest.java
@@ -1,8 +1,3 @@
-/*
- * Copyright Tidemark Systems, Inc. All rights reserved.
- * Tidemark Systems Confidential and Proprietary Information. Not for external distribution, use or sale.
- * Tidemark Systems software is exclusively licensed according to the terms of our Software License and Services Agreement.
- */
 package com.linkedin.norbert.javacompat.network;
 
 import java.util.HashSet;
