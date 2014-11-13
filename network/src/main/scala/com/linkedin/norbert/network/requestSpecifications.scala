814d71fd76de685a9aca58ac39e0d8d5cca1e151
Helen <hwoodward@hmc.edu>
2014-11-13 10:30:03 -0800

    Added converters between partitioned and non-partitioned request spec

--- network/src/main/scala/com/linkedin/norbert/network/requestSpecifications.scala
+++ network/src/main/scala/com/linkedin/norbert/network/requestSpecifications.scala
@@ -7,6 +7,10 @@ object RequestSpec {
   def apply[RequestMsg](message: RequestMsg): RequestSpec[RequestMsg] = {
     new RequestSpec(message);
   }
+
+  def apply[RequestMsg](partitionedSpec: PartitionedRequestSpec[RequestMsg, _]): RequestSpec[RequestMsg] = {
+    new RequestSpec(partitionedSpec.message.get);
+  }
 }
 
 class RequestSpec[RequestMsg](val message: RequestMsg) {
@@ -19,6 +23,9 @@ object PartitionedRequestSpec{
   def apply[RequestMsg, PartitionedId](message: Option[RequestMsg] = None,
                                        rb: Option[(Node, Set[PartitionedId]) => RequestMsg] = None): PartitionedRequestSpec[RequestMsg, PartitionedId] = {
     new PartitionedRequestSpec(message, rb)
+  }
+  def apply[RequestMsg, PartitionedId](requestSpec: RequestSpec[RequestMsg]): PartitionedRequestSpec[RequestMsg, PartitionedId] = {
+    new PartitionedRequestSpec(Some(requestSpec.message), None)
   }
 }
 
@@ -35,9 +42,11 @@ class PartitionedRequestSpec[RequestMsg, PartitionedId](val message: Option[Requ
 object testing {
   def main(args: Array[String]) {
     try {
-      var tester = RequestSpec[String]("test");
-      var partitionedTester = PartitionedRequestSpec[String, Int]();
-      println("no error");
+      val tester: RequestSpec[String] = RequestSpec[String]("test")
+      val partitionedTester: PartitionedRequestSpec[String, Int] = PartitionedRequestSpec[String, Int](tester)
+      val partitionedTester2: PartitionedRequestSpec[String, Int] = PartitionedRequestSpec[String, Int](Some("partitionedTest"))
+      val tester2: RequestSpec[String] = RequestSpec[String](partitionedTester)
+      println(tester2.message);
     }
     catch {
       case e: Exception => println("There was an exception: " + e)
