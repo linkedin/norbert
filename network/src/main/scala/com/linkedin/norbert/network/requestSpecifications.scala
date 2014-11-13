5ef26af60ac95d95540a95dc9ea430e5a8c6182a
Helen <hwoodward@hmc.edu>
2014-11-13 10:30:03 -0800

    Added converters between partitioned and non-partitioned request spec

--- network/src/main/scala/com/linkedin/norbert/network/requestSpecifications.scala
+++ network/src/main/scala/com/linkedin/norbert/network/requestSpecifications.scala
@@ -1,11 +1,19 @@
 
-
 package com.linkedin.norbert.network
 import com.linkedin.norbert.cluster.{ClusterException, Node}
+
+/**
+ * Insert proper comments for RequestSpec here
+ */
+
 
 object RequestSpec {
   def apply[RequestMsg](message: RequestMsg): RequestSpec[RequestMsg] = {
     new RequestSpec(message);
+  }
+
+  def apply[RequestMsg](partitionedSpec: PartitionedRequestSpec[RequestMsg, _]): RequestSpec[RequestMsg] = {
+    new RequestSpec(partitionedSpec.message.get);
   }
 }
 
@@ -13,12 +21,17 @@ class RequestSpec[RequestMsg](val message: RequestMsg) {
 
 }
 
-
+/**
+ * Insert proper comments for PartitionedRequestSpec here
+ */
 
 object PartitionedRequestSpec{
   def apply[RequestMsg, PartitionedId](message: Option[RequestMsg] = None,
                                        rb: Option[(Node, Set[PartitionedId]) => RequestMsg] = None): PartitionedRequestSpec[RequestMsg, PartitionedId] = {
     new PartitionedRequestSpec(message, rb)
+  }
+  def apply[RequestMsg, PartitionedId](requestSpec: RequestSpec[RequestMsg]): PartitionedRequestSpec[RequestMsg, PartitionedId] = {
+    new PartitionedRequestSpec(Some(requestSpec.message), None)
   }
 }
 
@@ -31,13 +44,18 @@ class PartitionedRequestSpec[RequestMsg, PartitionedId](val message: Option[Requ
 
 }
 
+/**
+ * Put comments on testing
+ */
 
 object testing {
   def main(args: Array[String]) {
     try {
-      var tester = RequestSpec[String]("test");
-      var partitionedTester = PartitionedRequestSpec[String, Int]();
-      println("no error");
+      val tester: RequestSpec[String] = RequestSpec[String]("test")
+      val partitionedTester: PartitionedRequestSpec[String, Int] = PartitionedRequestSpec[String, Int](tester)
+      val partitionedTester2: PartitionedRequestSpec[String, Int] = PartitionedRequestSpec[String, Int](Some("partitionedTest"))
+      val tester2: RequestSpec[String] = RequestSpec[String](partitionedTester)
+      println(tester2.message);
     }
     catch {
       case e: Exception => println("There was an exception: " + e)
