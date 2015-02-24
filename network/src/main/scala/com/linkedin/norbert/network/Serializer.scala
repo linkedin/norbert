a175b3645abd666ead42dc193257f53f547e8e69
Helen <hwoodward@hmc.edu>
2015-02-23 19:14:21 -0800

    defaulted priority in inputSerializer to 0

--- network/src/main/scala/com/linkedin/norbert/network/Serializer.scala
+++ network/src/main/scala/com/linkedin/norbert/network/Serializer.scala
@@ -39,7 +39,7 @@ trait OutputSerializer[-RequestMsg, -ResponseMsg] {
 
 trait InputSerializer[+RequestMsg, +ResponseMsg] {
   def requestName: String
-  def priority: Int
+  def priority: Int = 0
   def requestFromBytes(bytes: Array[Byte]): RequestMsg
   def responseFromBytes(bytes: Array[Byte]): ResponseMsg
 }
